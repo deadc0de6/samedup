6dee5a616b9042e38b045c8d69f56e16e81ce154
deadc0de6 <info@deadc0de.ch>
2024-01-23 14:02:49 +0100

    fix special chars

--- internal/reporter/script.go
+++ internal/reporter/script.go
@@ -7,18 +7,33 @@ package reporter
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/deadc0de6/samedup/internal/db"
+	"github.com/deadc0de6/samedup/internal/logger"
 )
 
 var (
-	header      = "#!/usr/bin/env bash\n#\n"
-	statsHeader = "# total %d duplicates, total wasted: %s\n#\n"
-	dupHeader   = "# %d duplicates for \"%s\" - would free %s\n"
-	dupFirst    = "#rm -fv '%s'\n"
-	dupDup      = "rm -fv '%s'\n"
-	crlf        = "\n"
+	header                 = "#!/usr/bin/env bash\n#\n"
+	statsHeader            = "# total %d duplicates, total wasted: %s\n#\n"
+	dupHeader              = "# %d duplicates for \"%s\" - would free %s\n"
+	dupFirst               = "#rm -fv '%s'\n"
+	dupDup                 = "rm -fv '%s'\n"
+	crlf                   = "\n"
+	specials               = []string{"$", "`", "\\", "!"}
+	singleQuoteReplacement = `'\''`
 )
+
+func escaper(path string) string {
+	newpath := path
+	for _, special := range specials {
+		replacement := fmt.Sprintf("\\%s", special)
+		newpath = strings.Replace(newpath, special, replacement, -1)
+	}
+	newpath = strings.Replace(newpath, "'", singleQuoteReplacement, -1)
+	logger.Debugf("sanitize \"%s\" to \"%s\"", path, newpath)
+	return newpath
+}
 
 func printScript(dups *db.Duplicates) {
 	fmt.Print(header)
@@ -33,7 +48,8 @@ func printScript(dups *db.Duplicates) {
 			if idx == 0 {
 				tmpl = dupFirst
 			}
-			l := fmt.Sprintf(tmpl, node.AbsPath)
+			sanitized := escaper(node.AbsPath)
+			l := fmt.Sprintf(tmpl, sanitized)
 			fmt.Print(l)
 		}
 		fmt.Print(crlf)
